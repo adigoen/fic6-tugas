3930652c8b96abeee0eabe77971dcd1131cb8d9e
adigun <adigunpde07@gmail.com>
2023-07-21 16:34:13 +0700

    callback

---
+++ src/api/payment-callback/controllers/payment-callback.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * payment-callback controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::payment-callback.payment-callback');
